ebba28d91651956e14d3dbe5dca2bce1b29d01db
Iverson Watler <iwatler@gmail.com>
2024-02-05 19:23:22 -0800

    Update HTML-CSS-Projects

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 function Sidebar(){
 
 const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';.
+    
+    const hideSidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
